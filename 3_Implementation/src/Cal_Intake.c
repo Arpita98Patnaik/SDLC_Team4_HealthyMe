f0235b87b5e58fed46093ccfdc9d99b3b9020c7f
Pallavi-2500 <pallavisharma0025.official@gmail.com>
2021-08-21 12:12:05 +0530

    Updated Cal_intake

---
+++ 3_Implementation/src/Cal_Intake.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<stdlib.h>
+void mealchoice()
+{
+    int brkfst_limit=0, lunch_limit=0, snack_limit =0,  dinner_limit =0, bmr;
+    int choice;
+    do
+    {
+        system("cls");
+        printf("________________________Choose your Meal plan_______________________\n");
+        printf("1. Fiesty Breakfast, Light lunch, Healthy dinner\n2.Healthy breakfast, Fiesty lunch, Light dinner\n3. Light Breakfast, Healthy lunch, Fiesty dinner\n");
+        fflush(stdin);
+        scanf("%d",&choice);
+		if(choice==1){
+            brkfst_priority();
+            }
+        else if (choice ==2){
+            lunch_priority();
+        }
+        else if (choice ==3){
+            dinner_priority();
+        }
+        else
+        {
+            printf("Choose a valid choice");
+            system("cls");
+        }
+    } while (choice<=1||choice>=3);
+    int brkfst_priority()
+    {
+        brkfst_limit = (40/100)*bmr;
+        lunch_limit = (20 / 100)*bmr;
+        snack_limit = (10/100)*bmr;
+        dinner_limit = (30/100)*bmr; 
+    }
+        int lunch_priority()
+    {
+        brkfst_limit = (30/100)*bmr;
+        lunch_limit = (40/100)*bmr;
+        snack_limit = (10/100)*bmr;
+        dinner_limit = (20/100)*bmr; 
+    }
+        int dinner_priority()
+    {
+        brkfst_limit = (20/100)*bmr;
+        lunch_limit = (30/100)*bmr;
+        snack_limit = (10/100)*bmr;
+        dinner_limit = (40/100)*bmr; 
+    }
+}
