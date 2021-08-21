14865421988c0839a6bf4a297bedb936e7943d3c
99Pp <paraspanda99@gmail.com>
2021-08-21 13:03:53 +0530

    calorie intake files

--- 3_Implementation/inc/dietPlan.h
+++ 3_Implementation/inc/dietPlan.h
@@ -1,26 +1,41 @@
 #ifndef __DIET_PLAN_H__
     
     #define __DIET_PLAN_H__
-    typedef struct {
+    typedef struct brkfst{
         char name[50];
         int cal;
+        struct brkfst *next;
     }brkfst;
 
-    typedef struct {
+    typedef struct lunch{
         char name[50];
         int cal;
+        struct lunch *next;
     }lunch;
 
-    typedef struct {
+    typedef struct snack{
         char name[50];
         int cal;
+        struct snack *next;
     }snack;
 
-    typedef struct {
+    typedef struct dinner{
         char name[50];
         int cal;
+        struct dinner *next;
     }dinner;
 
+    brkfst* makeBrkfstList(char*,brkfst*);
+    int printBrkfstList(brkfst*);
+
+    lunch* makeLunchList(char*,lunch*);
+    int printLunchList(lunch*);
+
+    snack* makeSnacksList(char*,snack*);
+    int printSnacksList(snack*);
+
+    dinner* makeDinnerList(char*,dinner*);
+    int printDinnerList(dinner*);
 
 
 
---
+++ 3_Implementation/src/dietPlan.c
@@ -0,0 +1,189 @@
+#include "dietPlan.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+brkfst* makeBrkfstList(char *path,brkfst *head){
+    
+    FILE *fp = fopen(path,"r");
+
+    if(fp == NULL){
+        printf("NULL");
+        return NULL;
+    }
+    brkfst *newnode,*temp; 
+    
+    while(!(feof(fp))){
+        char *info = malloc(1024);
+        newnode = (brkfst*)malloc(sizeof(brkfst));
+        newnode->cal = 0;
+        
+        fscanf(fp,"%49[^,],%d\n",newnode->name,&newnode->cal);
+        newnode->next = NULL;
+        
+        if(head==NULL){
+            head = newnode;
+        }
+        else{
+            temp = head;
+                while(temp->next!=NULL)
+                        temp = temp->next;
+            temp->next = newnode;   
+        }
+    }
+    fclose(fp) ;
+return head;
+}
+lunch* makeLunchList(char *path,lunch *head){
+    
+    FILE *fp = fopen(path,"r");
+
+    if(fp == NULL){
+        printf("NULL");
+        return NULL;
+    }
+    lunch *newnode,*temp; 
+    
+    while(!(feof(fp))){
+        char *info = malloc(1024);
+        newnode = (lunch*)malloc(sizeof(lunch));
+        newnode->cal = 0;
+        
+        fscanf(fp,"%49[^,],%d\n",newnode->name,&newnode->cal);
+        newnode->next = NULL;
+        
+        if(head==NULL){
+            head = newnode;
+        }
+        else{
+            temp = head;
+                while(temp->next!=NULL)
+                        temp = temp->next;
+            temp->next = newnode;   
+        }
+    } 
+    fclose(fp) ;
+return head;
+}
+snack* makeSnacksList(char *path,snack *head){
+    
+    FILE *fp = fopen(path,"r");
+
+    if(fp == NULL){
+        printf("NULL");
+        return NULL;
+    }
+    snack *newnode,*temp; 
+    
+    while(!(feof(fp))){
+        char *info = malloc(1024);
+        newnode = (snack*)malloc(sizeof(snack));
+        newnode->cal = 0;
+        
+        fscanf(fp,"%49[^,],%d\n",newnode->name,&newnode->cal);
+        newnode->next = NULL;
+        
+        if(head==NULL){
+            head = newnode;
+        }
+        else{
+            temp = head;
+                while(temp->next!=NULL)
+                        temp = temp->next;
+            temp->next = newnode;   
+        }
+    }
+    fclose(fp) ;
+return head;
+}
+dinner* makeDinnerList(char *path,dinner *head){
+    
+    FILE *fp = fopen(path,"r");
+
+    if(fp == NULL){
+        printf("NULL");
+        return NULL;
+    }
+    dinner *newnode,*temp; 
+    
+    while(!(feof(fp))){
+        char *info = malloc(1024);
+        newnode = (dinner*)malloc(sizeof(dinner));
+        newnode->cal = 0;
+        
+        fscanf(fp,"%49[^,],%d\n",newnode->name,&newnode->cal);
+        newnode->next = NULL;
+        
+        if(head==NULL){
+            head = newnode;
+        }
+        else{
+            temp = head;
+                while(temp->next!=NULL)
+                        temp = temp->next;
+            temp->next = newnode;   
+        }
+    }
+    fclose(fp) ;
+return head;
+}
+
+int printBrkfstList(brkfst *head){
+    if(head ==NULL){
+        return -1;
+    }
+    
+      brkfst* temp = head;
+
+      printf("\n");
+      while(temp!=NULL){
+            
+            printf("%-49s\t%d\n",temp->name,temp->cal);
+            temp = temp->next;
+      }
+      return 1;
+}
+int printLunchList(lunch *head){
+    if(head ==NULL){
+        return -1;
+    }
+    
+      lunch* temp = head;
+
+      printf("\n");
+      while(temp!=NULL){
+            
+            printf("%-49s\t%d\n",temp->name,temp->cal);
+            temp = temp->next;
+      }
+      return 1;
+}
+int printSnacksList(snack *head){
+    if(head ==NULL){
+        return -1;
+    }
+    
+      snack* temp = head;
+
+      printf("\n");
+      while(temp!=NULL){
+            
+            printf("%-49s\t%d\n",temp->name,temp->cal);
+            temp = temp->next;
+      }
+      return 1;
+}
+int printDinnerList(dinner *head){
+    if(head ==NULL){
+        return -1;
+    }
+    
+      dinner* temp = head;
+
+      printf("\n");
+      while(temp!=NULL){
+            
+            printf("%-49s\t%d\n",temp->name,temp->cal);
+            temp = temp->next;
+      }
+      return 1;
+}
