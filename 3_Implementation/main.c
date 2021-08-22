04f11547668f257ee8a0c9883354566e4bfefb29
99Pp <50335521+99Pp@users.noreply.github.com>
2021-08-22 23:59:43 +0530

    update main.c
    added exit codes

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,29 +1,93 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include<essentials.h>
+#include<bmi_bmr.h>
 #include<dietPlan.h>
 #include<Cal_Intake.h>
+#include<water.h>
+#include<workout.h>
+#define MAX_SIZE 100
 
 int main(){
-    char *path_b = "res\\brkfst.csv";
-    char *path_l = "res\\lunch.csv";
-    char *path_s = "res\\snacks.csv";
-    char *path_d = "res\\dinner.csv";
-    int bmr = 300;
-    brkfst *b_head = NULL;
-    b_head = makeBrkfstList(path_b,b_head);
-
-    lunch *l_head = NULL;
-    l_head = makeLunchList(path_l,l_head);
-
-    snack *s_head = NULL;
-    s_head = makeSnacksList(path_s,s_head);
-
-    dinner *d_head = NULL;
-    d_head = makeDinnerList(path_d,d_head);
-
-
-    
-
-    printf("%d",mealchoice(bmr,b_head,l_head,s_head,d_head));
-    
+    member ptr1[MAX_SIZE];
+	int index = 0, x = 0, mbid;
+    ret_code_t ecc = 0;
+	while(x!=3){
+		int choice;
+		printf("     Healthy  Me   \n");
+		printf(" -------------------------------------------------------------------\n");
+		printf("               1- New User           \n");
+		printf("               2- Existing User          \n");
+		printf("               3- Exit:          \n");
+		printf(" ------------------------------------------------------------------ \n");	
+		printf("  Enter your choice: ");
+		scanf("%d",&choice);
+		
+		switch(choice){
+		
+			case 1:
+				if(index<MAX_SIZE)
+				{
+					ecc = newmember(ptr1,index);
+					index++;
+					if(ecc!=SUCCESS)
+						return ecc;
+					printf("               Select one of your choices                           \n");
+					printf(" -------------------------------------------------------------------\n");
+					printf("               1- To Calculate your BMI and BMR value               \n");
+					printf("               2- To get a personalized diet plan report              \n");
+					printf("               3- To get a personalized water intake report           \n");
+					printf("               4- To get a personalized workout report               \n");
+					printf("               5- To get a personalized overall report      \n");
+					printf(" ------------------------------------------------------------------ \n");	
+					printf("  Enter your choice: ");
+					scanf("%d",&choice);
+                    ecc = utility(choice,ptr1,index);
+					if(ecc!=SUCCESS)
+						return ecc;
+				}
+				break;
+		  	case 2:
+		  		printf("Enter ID:\n");
+				scanf("%d",&mbid);
+				if(mbid>index){
+					printf("User Not availiable\n");
+					return INCONSISTENT_DATA;
+				}
+				printf("               Select one of your choices                           \n");
+				printf(" -------------------------------------------------------------------\n");
+				printf("               1- To Calculate your BMI and BMR value               \n");
+				printf("               2- To get a personalized diet plan report              \n");
+				printf("               3- To get a personalized water intake report           \n");
+				printf("               4- To get a personalized workout report               \n");
+				printf("               5- To get a personalized overall report      \n");
+				printf("               6- To see your Old Report      \n");
+				printf(" ------------------------------------------------------------------ \n");	
+				printf("  Enter your choice: ");
+				scanf("%d",&choice);
+				switch(choice){
+					case 1:
+					case 2:
+					case 3:
+					case 4:
+					case 5:
+						ecc = utility(choice,ptr1,mbid);
+						if(ecc!=SUCCESS)
+							return ecc;
+						break;
+					case 6:
+						ecc = displaymemberdetails(ptr1,mbid);
+						if(ecc!=SUCCESS)
+							return ecc;
+						break;
+				}
+		  	case 3:
+		  		x=3;
+		  		break;
+		  	default:
+		  		printf("Incorrect Entry\n");
+            	return INCONSISTENT_DATA;
+            
+		  }
+	}
+		  
+	return SUCCESS;
 }
