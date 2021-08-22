9d0aab7acdc64f2c719431ca7c76bc426cfee4f4
ManasaKarur <86444475+ManasaKarur@users.noreply.github.com>
2021-08-23 00:02:34 +0530

    Add files via upload

---
+++ 3_Implementation/inc/workout.h
@@ -0,0 +1,10 @@
+#ifndef __WORKOUT_H__
+    #define __WORKOUT_H__
+    #include<essentials.h>
+     ret_code_t beginner();
+     ret_code_t intermediate();
+     ret_code_t advanced();
+     ret_code_t workout(member*,int);
+   
+    
+#endif
