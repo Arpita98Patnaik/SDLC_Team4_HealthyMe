a203ac28b6e98733afe05f01c3c8b54d5caae3a5
AjV1999 <ajayvictor18@gmail.com>
2021-08-22 20:32:16 +0530

    test

--- 3_Implementation/Water_bmi/bmi_bmr.c
+++ 3_Implementation/Water_bmi/bmi_bmr.c
@@ -19,12 +19,20 @@ ret_code_t calcBMI(struct member *p, int n)
 	{
 		weight = (p+n)->w;
 		height = (p+n)->h;
+        if(weight>150 || height>250)
+        {
+            return INCONSISTENT_DATA;
+        }
 		bmi = (weight /(height*height))*10000;
 	}
 	else
 	{
 		weight = ((p+n)->w)*0.45359237;
 		height = ((p+n)->h)*30.48;
+        if(weight>150 || height>250)
+        {
+            return INCONSISTENT_DATA;
+        }
 		bmi = (weight /(height*height))*10000;
 	}
 	
@@ -66,11 +74,19 @@ ret_code_t calcBMR(struct member *p, int n)
 	{
 		weight = (p+n)->w;
 		height = (p+n)->h;
+        if(weight>150 || height>250)
+        {
+            return INCONSISTENT_DATA;
+        }
 	}
 	else
 	{
 		weight = ((p+n)->w)*0.45359237;
 		height = ((p+n)->h)*30.48;
+        if(weight>150 || height>250)
+        {
+            return INCONSISTENT_DATA;
+        }
 	}
 	bmr = 0;
 
@@ -80,7 +96,7 @@ ret_code_t calcBMR(struct member *p, int n)
 	else if (g == 2) {
 		bmr = (10*weight) + (6.25 * height) - (5*a) + 5;
 	}
-	//Prin
+	//Print
 	printf( "You have a Basal Metabolic Rate (BMR) of:%lf ",bmr);
 	printf(" calories per day.\n");
 	return SUCCESS;
