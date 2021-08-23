d6c7aa42847c5e22063bc21f8744674373fa64b9
Varna-M <varnamadhu@cet.ac.in>
2021-08-23 10:17:19 +0530

    updated essentials

---
+++ 3_Implementation/inc/essentials.h
@@ -0,0 +1,35 @@
+#ifndef __ESSENTIALS_H__
+
+    #define __ESSENTIALS_H__
+    #include<stdio.h>
+    #include<stdlib.h>
+    #include<math.h>
+    #include<string.h>
+
+    typedef struct member
+        {
+            int usr_id;
+            char name[30];
+            int age;
+            int gender;
+            int unit;
+            float w;
+            float h;
+            int time[2];
+            float total_w;
+            float bmi;
+            int bmr;
+        }member;
+    
+    typedef enum ret_code{
+        NULL_PTR = -1,   /** Error code for NULL pointer */
+        SUCCESS,         /** Value for Success */
+        NULL_FILE_PTR,   /** Error code for Null FILE Pointer */
+        INCONSISTENT_DATA /** Error Code for improper input data */
+    }ret_code_t;
+
+    ret_code_t displaymemberdetails( struct member*, int );
+	ret_code_t newmember(struct member*,int);
+    ret_code_t utility(int,member*,int);
+
+#endif
