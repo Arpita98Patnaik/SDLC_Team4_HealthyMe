57d1403b1e1e45f70e40c4b5683b57fb9b25048d
Madhura M <madhura.26m@gmail.com>
2021-08-23 10:08:54 +0530

    uploaded utility file

---
+++ 3_Implementation/src/utility.c
@@ -0,0 +1,86 @@
+#include<essentials.h>
+#include<dietPlan.h>
+#include<Cal_Intake.h>
+#include<bmi_bmr.h>
+#include<water.h>
+#include<workout.h>
+ret_code_t utility(int choice,member *ptr, int n){
+    ret_code_t ecc = 0; 
+    switch(choice){
+        case 1:                     
+            ecc = calcBMI(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc = calcBMR(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            // report_builder(ptr, string_module);   //string module to be sent
+            break;
+
+        case 2:  
+            ecc = calcBMR(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc = calorie(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            break;
+        case 3:
+            ecc =  calcwaterquant(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc =  workoutcheck( ptr,n );
+            if(ecc!=SUCCESS)
+                return ecc;
+            //  report_builder(ptr,string_module);  //string module to be sent
+            break;
+
+        case 4:
+            ecc = calcBMI(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc = workout(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            // report_builder(ptr,string_module);  //string module to be sent
+            break;
+
+        case 5:   
+            ecc = calcBMI(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc = calcBMR(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc = calorie(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc = workout(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+
+            ecc =  calcwaterquant(ptr,n);
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            ecc =  workoutcheck( ptr,n );
+            if(ecc!=SUCCESS)
+                return ecc;
+            
+            //  report_builder(ptr,string_module);  //string module to be sent
+            break;
+
+        default:
+            printf("Incorrect Entry\n");
+            return INCONSISTENT_DATA;
+            break;
+    }
+    return SUCCESS;
+}
