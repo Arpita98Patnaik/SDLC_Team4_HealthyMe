9ce251f5b200831cf2bf91496e95140f38730f16
262795 <gopikahnair98@gmail.com>
2021-08-23 11:15:41 +0530

    new commit

---
+++ 3_Implementation/src/essentials.c
@@ -0,0 +1,85 @@
+#include<essentials.h>
+
+ret_code_t newmember(struct member *p, int n ){
+    if(p==NULL)
+        return NULL_PTR;
+
+	int ch_u,a,g;
+	float weight,height;
+	(p+n)->usr_id = n;
+	
+    printf("Enter your Name: ");
+	scanf("%29s",(p + n)->name);
+	
+    printf("Enter your Age: ");
+	scanf("%d",&a);
+	(p+n)->age =a;
+	
+    printf("Select gender: \n");
+	printf("1. Female\n");
+	printf("2. Male\n");
+	printf("Enter your choice: ");
+	scanf("%d",&g);
+	(p+n)->gender = g;
+	
+    printf("Select a unit of your preference:\n");
+	printf("   1. SI units(kgs)    ");
+	printf("   2. Imperial(Pounds)    ");
+	scanf("%d",&ch_u);
+	
+    if(ch_u == 1)
+	{
+		(p + n)->unit = 1;
+		printf("Enter your weight in Kgs:");
+		scanf("%f",&weight);
+		(p+n)->w = weight;
+		printf("Enter height in meters:");
+		scanf("%f",&height);
+		(p+n)->h = height;
+        if(weight>150 || height>250)
+        {
+            return INCONSISTENT_DATA;
+        }
+	}
+	else
+	{
+		(p +n)->unit =2;
+		printf("Enter your weight in Pounds:");
+		scanf("%f",&weight);
+		(p+n)->w = weight;
+		//printf("%f, Weight",(p+n)->w);
+		printf("Enter height in feet:");
+		scanf("%f",&height);
+		(p+n)->h = height;
+        if(weight>1000 || height>6)
+        {
+            return INCONSISTENT_DATA;
+        }
+	}
+    return SUCCESS;
+		
+}
+
+
+ret_code_t displaymemberdetails( struct member *p, int n)
+{
+    if(p==NULL)
+        return NULL_PTR;
+
+	printf("-------------------------------------------------\n");
+	printf("Member ID: %d\n",(p+n)->usr_id);
+	printf("Member Name: %s\n",(p+n)->name);
+	printf("Member age: %d\n",(p+n)->age);
+	if((p+n)->unit == 1)
+	{
+		printf("Units used: SI units\n");
+	}
+	else if((p+n)->unit == 2)
+	{
+		printf("Units used: Imperial units\n");
+	}
+	printf("Weight: %f\n",(p+n)->w);
+	printf("Workout duration: %d:%d\n",(p+n)->time[0],(p+n)->time[1]);
+	printf("Recomended Water-intake: %f\n",(p+n)->total_w);
+    return SUCCESS;
+}
